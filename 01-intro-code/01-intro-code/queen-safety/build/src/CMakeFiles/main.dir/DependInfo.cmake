7c42e31a9221b7eb070e70e3ea2cde5c5e2ca5fd
tangke <509634578tk@gmail.com>
2021-01-06 21:35:11 +0800

    failed to cmake of queen project

---
+++ 01-intro-code/01-intro-code/queen-safety/build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ketang/stanfordcs106x/01-intro-code/01-intro-code/queen-safety/src/queen-safety-graphics.cpp" "/Users/ketang/stanfordcs106x/01-intro-code/01-intro-code/queen-safety/build/src/CMakeFiles/main.dir/queen-safety-graphics.cpp.o"
+  "/Users/ketang/stanfordcs106x/01-intro-code/01-intro-code/queen-safety/src/queen-safety.cpp" "/Users/ketang/stanfordcs106x/01-intro-code/01-intro-code/queen-safety/build/src/CMakeFiles/main.dir/queen-safety.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
